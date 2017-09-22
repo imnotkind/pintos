2c626ee0814fa340d3d4bbff83f5b05fa9b8ed7a
imnotkind <billy307@daum.net>
2017-09-22 23:04:29 +0900

    jh

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -72,8 +72,9 @@ timer_ticks (void)
 {
   enum intr_level old_level = intr_disable ();
   int64_t t = ticks;
-  intr_set_level (old_level);
   thread_wake();
+  intr_set_level (old_level);
+ 
   return t;
 }
 
