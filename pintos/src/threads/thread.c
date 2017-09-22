3224739f2f91faefb801e256e5af5fd9d63fe401
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-09-23 04:02:45 +0900

    change allelem to elem

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -359,7 +359,7 @@ thread_wake (int64_t current_ticks)
   struct list_elem *e;
   printf("X");
   for (e = list_begin (&sleep_list); e != list_end (&sleep_list); e = list_next (e)){
-      struct thread *t = list_entry(e, struct thread, allelem);
+      struct thread *t = list_entry(e, struct thread, elem);
       if((*t).wake_ticks > current_ticks){
         if(list_empty(&sleep_list)){
           next_wake_ticks = INT64_MAX;
