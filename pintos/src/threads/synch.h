29ff99f05fe010ad9280dfe8d3f86a8e5438b986
imnotkind <billy307@daum.net>
2017-09-23 18:47:45 +0900

    as

--- pintos/src/threads/synch.h
+++ pintos/src/threads/synch.h
@@ -8,7 +8,7 @@
 struct semaphore 
   {
     unsigned value;             /* Current value. */
-    struct list waiters;        /* List of waiting threads. */
+    struct list waiters;        /* List of waiting threads. (blocked threads)  */
   };
 
 void sema_init (struct semaphore *, unsigned value);
