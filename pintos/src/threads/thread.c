4a347635545df7b196af989be26ce906c6609d2c
imnotkind <billy307@daum.net>
2017-09-23 15:57:34 +0900

    alarm-single maybe complete

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -372,8 +372,8 @@ thread_sleep (int64_t wake_ticks)
   thread_list_stat();
 
   thread_block();
-  thread_list_stat();
   printf("<%s> : TID %d back to life\n",cur->name, cur->tid);
+  thread_list_stat();
   intr_set_level(old_level);
 }
 
@@ -387,26 +387,26 @@ thread_wake (int64_t current_ticks)
       printf("CHECKING <%s> : TID %d\n",t->name,t->tid);
       if( t->wake_ticks > current_ticks){
         printf("No more ticks to unblock\n");
-        if(list_empty(&sleep_list)){
-          printf("list is empty\n");
-          next_wake_ticks = INT64_MAX;
-        }
-        else{
-          next_wake_ticks = t->wake_ticks;
-        }
+        next_wake_ticks = t->wake_ticks;
         break;
       }
       else{
         printf("I WILL NOW TRY TO UNBLOCK <%s> : TID %d\n",t->name,t->tid);
         thread_list_stat();
-        e = list_remove(e);
+        e = list_remove(e); //if we don't remove from list before unblock, list gets corrupted
         printf("REMOVE <%s> : TID %d from SLEEP LIST\n",t->name,t->tid);
         thread_list_stat();
         thread_unblock(t);
         printf("UNBLOCK SUCCESS!\n");
         thread_list_stat();
+
       }
       
+  }
+  
+  if(list_empty(&sleep_list)){
+    printf("list is empty\n");
+    next_wake_ticks = INT64_MAX;
   }
   
 }
