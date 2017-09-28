5b71f95f757df2fd363fda0092178a8c1aa42adf
imnotkind <billy307@daum.net>
2017-09-29 05:59:46 +0900

    gq

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -100,7 +100,7 @@ thread_init (void)
   list_init (&ready_list);
   list_init (&sleep_list);
   list_init (&all_list);
-  }
+  
 
   /* Set up a thread structure for the running thread. */
   initial_thread = running_thread ();
