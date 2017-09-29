6bbda2173a3302bdaf13b1365ba602ace36463ec
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-09-29 14:29:13 +0900

    ready_list pointer mistake fix

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -620,7 +620,7 @@ void mlfqs_sort(void)
 
 void mlfqs_schedule(void)
 {
-  if(!list_empty(ready_list))
+  if(!list_empty(&ready_list))
     if(thread_current()->priority < list_entry(list_front(ready_list), struct thread, elem)->priority);
       schedule();
 }
