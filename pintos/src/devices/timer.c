4dcaeae393c44292d0ba403b08519a25ebfc61b7
imnotkind <billy307@daum.net>
2017-09-29 16:57:30 +0900

    qgw

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -184,7 +184,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
   
   
   if(thread_mlfqs == 1){
-    thread_foreach(&recent_cpu_inc,NULL);
+    recent_cpu_inc(thread_current());
     if(ticks % TIMER_FREQ == 0){
       calc_load_avg();
       thread_foreach(&calc_recent_cpu,NULL);
