39044e958ed5d7a175daff65386b2af746f6918d
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-10-20 22:35:00 +0900

    fix err?

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -113,7 +113,7 @@ start_process (void *file_name_)
     *(int *)if_.esp = argc;
     if_.esp -= 4;
     *(int *)if_.esp = 0; // return_address
-    for(i = 0; i < debug - esp; i++){
+    for(i = 0; i < (int)(debug - if_.esp); i++){
       printf("%c", &(if_.esp + i));
     }
 
