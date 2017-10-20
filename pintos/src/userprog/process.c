610f19915e775353060138f9164a8c78368e51ba
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-10-20 20:24:33 +0900

    debugg

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -91,6 +91,7 @@ start_process (void *file_name_)
     printf("debuging one...\n");
     for(i = argc - 1; i >= 0; i--){
       if_.esp -= strlen(argv[i]) + 1;
+      printf("argv[%d]: %s\n", i, argv[i]);
       strlcpy (if_.esp, argv[i], strlen(argv[i]) + 1);
     }
 
