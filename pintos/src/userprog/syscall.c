7ca8ecf712e925d3c1d3e7a1900c67f95092346f
imnotkind <billy307@daum.net>
2017-10-28 22:22:19 +0900

    syscall interface

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -20,5 +21,32 @@ syscall_handler (struct intr_frame *f)
   //address safety checking needed
   int sysno = *p;
   printf ("no %d",sysno);
+  switch(sysno)
+  {
+    case SYS_HALT:                   /* Halt the operating system. */
+    case SYS_EXIT:                   /* Terminate this process. */
+    case SYS_EXEC:                   /* Start another process. */
+    case SYS_WAIT:                   /* Wait for a child process to die. */
+    case SYS_CREATE:                 /* Create a file. */
+    case SYS_REMOVE:                 /* Delete a file. */
+    case SYS_OPEN:                   /* Open a file. */
+    case SYS_FILESIZE:               /* Obtain a file's size. */
+    case SYS_READ:                   /* Read from a file. */
+    case SYS_WRITE:                  /* Write to a file. */
+    case SYS_SEEK:                   /* Change position in a file. */
+    case SYS_TELL:                   /* Report current position in a file. */
+    case SYS_CLOSE:                  /* Close a file. */
+
+    /* Project 3 and optionally project 4. */
+    case SYS_MMAP:                   /* Map a file into memory. */
+    case SYS_MUNMAP:                 /* Remove a memory mapping. */
+
+    /* Project 4 only. */
+    case SYS_CHDIR:                  /* Change the current directory. */
+    case SYS_MKDIR:                  /* Create a directory. */
+    case SYS_READDIR:                /* Reads a directory entry. */
+    case SYS_ISDIR:                  /* Tests if a fd represents a directory. */
+    case SYS_INUMBE:                 /* Returns the inode number for a fd. */
+  }
   thread_exit ();
 }
