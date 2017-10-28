180b1a4af03b9e3f3f6db138da888f609a13fc22
imnotkind <billy307@daum.net>
2017-10-29 04:18:11 +0900

    syswrite start

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -18,12 +18,14 @@ syscall_handler (struct intr_frame *f)
 {
   printf ("system call!\n");
   int *p = f->esp;
-  //address safety checking needed
+  //address safety checking needed !!!!!!!!!!!!!
   int sysno = *p;
   printf ("no %d",sysno);
   switch(sysno)
   {
     case SYS_HALT:                   /* Halt the operating system. */
+      shutdown_power_off();
+      break;
     case SYS_EXIT:                   /* Terminate this process. */
     case SYS_EXEC:                   /* Start another process. */
     case SYS_WAIT:                   /* Wait for a child process to die. */
@@ -33,6 +35,13 @@ syscall_handler (struct intr_frame *f)
     case SYS_FILESIZE:               /* Obtain a file's size. */
     case SYS_READ:                   /* Read from a file. */
     case SYS_WRITE:                  /* Write to a file. */
+    {
+      int fd = *(int *)(p+1);
+      char * buffer = *(char **)(p+2);
+      unsigned int size = *(int *)(p+3);
+      printf("fd : %d ",fd);
+    }
+
     case SYS_SEEK:                   /* Change position in a file. */
     case SYS_TELL:                   /* Report current position in a file. */
     case SYS_CLOSE:                  /* Close a file. */
@@ -46,7 +55,7 @@ syscall_handler (struct intr_frame *f)
     case SYS_MKDIR:                  /* Create a directory. */
     case SYS_READDIR:                /* Reads a directory entry. */
     case SYS_ISDIR:                  /* Tests if a fd represents a directory. */
-    case SYS_INUMBE:                 /* Returns the inode number for a fd. */
+    case SYS_INUMBER:                 /* Returns the inode number for a fd. */
   }
   thread_exit ();
 }
