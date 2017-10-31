2090b2a08b1d6dabc0d4b36b7acd1c2aeb086bd6
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-11-01 00:45:48 +0900

    exception.c fix

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -148,6 +148,11 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+  if(not_present)
+  {
+    thread_current()->exit_code=-1;
+    thread_exit();
+  }
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
