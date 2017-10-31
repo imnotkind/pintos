43bd53a9816cc059a3b23feb72f0884060e7dc96
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-11-01 01:03:33 +0900

    gwa

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -4,7 +4,7 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
-#include <syscall.h>
+#include "userprog/syscall.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -151,7 +151,7 @@ page_fault (struct intr_frame *f)
 
   if(not_present)
   {
-    exit(-1); //syscall exit
+    sys_exit(-1); //syscall exit
   }
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
