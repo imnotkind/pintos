ac613a228fdc79b94dc17263104c9ed4d923e2bc
imnotkind <billy307@daum.net>
2017-11-03 00:20:27 +0900

    fw

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -33,6 +33,7 @@ process_execute (const char *file_name)
 {
   char *fn_copy, *fn_pure, *save_ptr;
   tid_t tid;
+  struct file *fp;
 
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
@@ -46,6 +47,10 @@ process_execute (const char *file_name)
   strlcpy (fn_pure, file_name, sizeof(char) * (strlen(file_name)+1));
   fn_pure = strtok_r(fn_pure, " " ,&save_ptr);
 
+  fp = filesys_open(fn_pure);
+  if(fp == NULL){
+    return -1;
+  }
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (fn_pure, PRI_DEFAULT, start_process, fn_copy);
 
