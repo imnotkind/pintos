83f18b4ef86224de34cb9f3276e43146949d9af5
imnotkind <billy307@daum.net>
2017-11-13 23:49:40 +0900

    spt

--- src/vm/page.h
+++ src/vm/page.h
@@ -3,10 +3,21 @@
 
 #include <list.h>
 
+enum page_type
+{
+    PAGE_FILE
+};
+
 struct sp_table_pack //sup page table
 {
+    struct thread * owner;
     void *upage;
     bool is_loaded;
+    
+    struct file * file;
+    
+    enum page_type type;
+
     struct list_elem elem;
 };
 
