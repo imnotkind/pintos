a42b045bb16ab0f846d261688db45a99a386c710
imnotkind <billy307@daum.net>
2017-11-15 00:52:36 +0900

    add to sp[age table remove

--- src/vm/page.c
+++ src/vm/page.c
@@ -25,35 +25,17 @@ struct sp_table_pack //sup page table
 };
 */
 
-void add_to_spage_table(void *upage, bool is_loaded)
+
+void add_file_to_spage_table(void *upage,struct file * file, off_t offset, uint32_t read_bytes, uint32_t zero_bytes, bool writable)
 {
    struct thread *cur = thread_current(); 
    struct sp_table_pack *spt;
    spt = (struct sp_table_pack *) malloc(sizeof(struct sp_table_pack));
+   ASSERT(spt != NULL);
+
    spt->owner = cur;
    spt->upage = upage;
-   spt->is_loaded = is_loaded;
-   
-   spt->file = NULL;
-   spt->offset = 0;
-   spt->read_bytes = 0;
-   spt->zero_bytes = 0;
-   spt->writable = false;
-   spt->type = PAGE_NULL;
-
-   lock_acquire(&cur->sp_table_lock);
-   list_push_back(&cur->sp_table, &spt->elem);
-   lock_release(&cur->sp_table_lock);
-}
-
-void add_file_to_spage_table(void *upage, bool is_loaded, struct file * file, off_t offset, uint32_t read_bytes, uint32_t zero_bytes, bool writable)
-{
-   struct thread *cur = thread_current(); 
-   struct sp_table_pack *spt;
-   spt = (struct sp_table_pack *) malloc(sizeof(struct sp_table_pack));
-   spt->owner = cur;
-   spt->upage = upage;
-   spt->is_loaded = is_loaded;
+   spt->is_loaded = false;
    
    spt->file = file;
    spt->offset = offset;
