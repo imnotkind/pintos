de8582e6a92944e4c5f627507fce57b64f00c976
imnotkind <billy307@daum.net>
2017-11-14 23:59:36 +0900

    wga

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -148,11 +148,11 @@ page_fault (struct intr_frame *f)
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
-
+/*
   if(not_present)
   {
     sys_exit(-1); //syscall exit
-  }
+  }*/
 
   check_addr_safe(fault_addr);
   /* To implement virtual memory, delete the rest of the function
