bc912608fa5f652485b807db8df4e9cda7d9999c
Changesong <changesong@postech.ac.kr>
2017-11-15 23:40:06 +0900

    load_file

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,11 +1,15 @@
 #include "vm/page.h"
 #include <stdio.h>
 #include <debug.h>
+#include <string.h>
 #include "threads/synch.h"
 #include "threads/thread.h"
 #include "threads/malloc.h"
 #include "threads/palloc.h"
-
+#include "threads/vaddr.h"
+#include "filesys/file.h"
+#include "userprog/process.h"
+#include "vm/frame.h"
 
 /*
 struct sp_table_pack //sup page table
@@ -13,13 +17,13 @@ struct sp_table_pack //sup page table
     struct thread * owner;
     void *upage;
     bool is_loaded;
-    
+
     struct file * file;
     off_t offset;
     uint32_t read_bytes;
     uint32_t zero_bytes;
     bool writable;
-    
+
     enum page_type type;
 
     struct list_elem elem;
@@ -29,7 +33,7 @@ struct sp_table_pack //sup page table
 
 bool add_file_to_spage_table(void *upage,struct file * file, off_t offset, uint32_t read_bytes, uint32_t zero_bytes, bool writable)
 {
-   struct thread *cur = thread_current(); 
+   struct thread *cur = thread_current();
    struct sp_table_pack *spt;
    spt = (struct sp_table_pack *) malloc(sizeof(struct sp_table_pack));
    if(spt == NULL)
@@ -38,7 +42,7 @@ bool add_file_to_spage_table(void *upage,struct file * file, off_t offset, uint3
    spt->owner = cur;
    spt->upage = upage;
    spt->is_loaded = false;
-   
+
    spt->file = file;
    spt->offset = offset;
    spt->read_bytes = read_bytes;
@@ -56,7 +60,7 @@ bool add_file_to_spage_table(void *upage,struct file * file, off_t offset, uint3
 void free_page_frame(void *upage) // page is kv_adrr
 {
     struct sp_table_pack *sp_table;
-    struct thread *cur = thread_current(); 
+    struct thread *cur = thread_current();
     //maybe we should start lock on this line
     sp_table = upage_to_sp_table_pack(upage);
     ASSERT(sp_table);
@@ -64,7 +68,7 @@ void free_page_frame(void *upage) // page is kv_adrr
     lock_acquire(&cur->sp_table_lock);
     list_remove(&sp_table->elem);
     lock_release(&cur->sp_table_lock);
-        
+
     palloc_free_page(upage);
     free(sp_table);
 }
@@ -74,7 +78,7 @@ struct sp_table_pack * upage_to_sp_table_pack(void * upage)
 {
     struct list_elem *e;
     struct sp_table_pack *sptp;
-    struct thread *cur = thread_current(); 
+    struct thread *cur = thread_current();
     for(e = list_begin(&cur->sp_table); e != list_end(&cur->sp_table); e = list_next(e))
     {
         sptp = list_entry(e, struct sp_table_pack, elem);
@@ -82,30 +86,31 @@ struct sp_table_pack * upage_to_sp_table_pack(void * upage)
             return sptp;
         }
     }
-    
+
     return NULL;
 }
 
 bool load_file(struct sp_table_pack * sptp)
 {
-    
-    /* Get a page of memory. */
-      uint8_t *kpage = alloc_page_frame (PAL_USER);
-      if (kpage == NULL)
-        return false;
+  size_t page_read_bytes = sptp->read_bytes < PGSIZE ? read_bytes : PGSIZE;
+  size_t page_zero_bytes = PGSIZE - page_read_bytes;
+  /* Get a page of memory. */
+  uint8_t *kpage = alloc_page_frame (PAL_USER);
+  if (kpage == NULL)
+    return false;
 
-      /* Load this page. */
-      if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
-        {
-          free_page_frame (kpage);
-          return false;
-        }
-      memset (kpage + page_read_bytes, 0, page_zero_bytes);
+  /* Load this page. */
+  if (file_read (sptp->file, kpage, page_read_bytes) != (int) page_read_bytes)
+    {
+      free_page_frame (kpage);
+      return false;
+    }
+  memset (kpage + page_read_bytes, 0, page_zero_bytes);
 
-      /* Add the page to the process's address space. */
-      if (!install_page (upage, kpage, writable))
-        {
-          free_page_frame (kpage);
-          return false;
-        }
+  /* Add the page to the process's address space. */
+  if (!install_page (upage, kpage, writable))
+    {
+      free_page_frame (kpage);
+      return false;
+    }
 }
