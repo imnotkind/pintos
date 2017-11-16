488cb437ac895a1959219dfa690e94aeb96eb301
imnotkind <billy307@daum.net>
2017-11-16 14:57:43 +0900

    wahg

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -153,7 +153,7 @@ page_fault (struct intr_frame *f)
   user = (f->error_code & PF_U) != 0;
 
   if(!check_addr_safe(fault_addr,1))
-    sys_exit();
+    sys_exit(-1);
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
