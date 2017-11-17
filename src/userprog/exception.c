9e343da71ae4e0f2a48ce8809dcf2cdbe87ff9c6
imnotkind <billy307@daum.net>
2017-11-17 15:54:06 +0900

    awhog

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -168,10 +168,9 @@ page_fault (struct intr_frame *f)
 
   if(sptp == NULL) //stack growth situation!, but later...
   {
-    if((void*)0x08048000 > fault_addr)
+    if(((void*)0x08048000 > fault_addr) || (fault_addr < f->esp - (void*)32))
       sys_exit(-1);
-    if(fault_addr >= f->esp - (void*)32)
-      page_load_success = grow_stack(fault_addr);
+    page_load_success = grow_stack(fault_addr);
   }
   else
   {
