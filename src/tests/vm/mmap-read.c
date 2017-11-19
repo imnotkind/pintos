a41e04e5763ba06961975f77520ff567ca419315
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-11-20 02:39:15 +0900

    test

--- src/tests/vm/mmap-read.c
+++ src/tests/vm/mmap-read.c
@@ -26,7 +26,9 @@ test_main (void)
     if (actual[i] != 0)
       fail ("byte %zu of mmap'd region has value %02hhx (should be 0)",
             i, actual[i]);
-
+  printf("Test Before MUNMAP");
   munmap (map);
+  printf("Test After MUNMAP");
+
   close (handle);
 }
