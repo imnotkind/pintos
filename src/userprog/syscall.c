3f2d6d3a7a5d438f0c1349dd5e8b5916e3b406b1
imnotkind <billy307@daum.net>
2017-11-19 23:49:40 +0900

    hwa

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -177,7 +177,7 @@ syscall_handler (struct intr_frame *f)
     {
       check_addr_safe(p+1,0,NULL);
       check_addr_safe(p+2,0,NULL);
-      check_addr_safe((void *)*(p+2),0,NULL);
+      //check_addr_safe((void *)*(p+2),0,NULL);
       check_addr_safe(p+3,0,NULL);
       int fd = *(int *)(p+1);
       void *buffer = *(void **)(p+2);
