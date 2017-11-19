1a61e2be00c4fb28c1e255db81f5d0601730b9f7
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-11-20 05:02:01 +0900

    include

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -6,6 +6,7 @@
 #include "threads/thread.h"
 #include "userprog/syscall.h"
 #include "vm/page.h"
+#include "threads/vaddr.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
