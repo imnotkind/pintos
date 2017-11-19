cd5100eac8eba1c28d138d273bcd7258ed0cdf11
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-11-20 05:01:25 +0900

    test

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -152,7 +152,7 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  if(!not_present && write) //not present check : pt-write-code test
+  if(!not_present || !is_user_vaddr(fault_addr) || fault_addr < 0x08048000 + 1) //not present check : pt-write-code test
   {
     sys_exit(-1);
   }
