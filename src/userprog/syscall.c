692be10caf42e03cc45c1ed853beba2fb4b7d6a9
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-11-21 02:00:40 +0900

    keep going

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -369,7 +369,7 @@ syscall_handler (struct intr_frame *f)
       struct flist_pack *fe = fd_to_flist_pack(fd);
       bool escape = false;
 
-      if(!upage || !is_user_vaddr(upage) || upage <  0x08048000 || upage < f->esp - 128 || pg_ofs(upage) != 0){
+      if(!upage || !is_user_vaddr(upage) || upage <  0x08048000 || upage < f->esp - 256 || pg_ofs(upage) != 0){
         f->eax = -1;
         break;
       }
