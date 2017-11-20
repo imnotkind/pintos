72df19e850c9e32bce05a198bab2be29cb6ce970
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-11-21 01:00:56 +0900

    kh njb

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -358,7 +358,7 @@ syscall_handler (struct intr_frame *f)
     case SYS_MMAP:                   /* Map a file into memory. */     
     {
       check_addr_safe(p+1,0,NULL);
-      check_addr_safe(p+2,0,NULL);
+      //check_addr_safe(p+2,0,NULL);
       int fd = *(int *)(p+1);
       void *buffer = *(void **)(p+2);
       void *upage = buffer;
