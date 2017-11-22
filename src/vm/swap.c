5db78e285daac3b3583fa8ef5b5279d33daa1cc6
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-11-23 04:32:43 +0900

    swap in and index_to_swap_table_pack

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -7,14 +7,13 @@ extern struct lock filesys_lock;
 
 void init_swap_table()
 {
+    int i;
     swap_block = block_get_role(BLOCK_SWAP);
     if(!swap_block){
         return;
     }
-
-    int i=0;
-    for(i; i < block_size(swap_block)*BLOCK_SECTOR_SIZE/PGSIZE; i++)
-    {
+    
+    for(i = 0; i < block_size(swap_block)* BLOCK_SECTOR_SIZE/PGSIZE; i++){
         struct swap_table_pack * stp = malloc(sizeof(struct swap_table_pack));
         //additional init
     }
@@ -22,6 +21,53 @@ void init_swap_table()
     list_init(&swap_table);
     lock_init(&swap_lock);
     lru_pos = list_begin(&swap_table);
+}
+
+//swap into memory with index
+void swap_in(size_t index, void *upage)
+{
+    int i;
+    struct swap_table_pack *stp;
+
+	lock_acquire(&swap_lock);
+	stp = index_to_swap_table_pack(index);
+	
+	if (stp == NULL || stp->recent_used == true){ //it was 'available' in cctv. we need check
+		lock_release(&swap_lock);
+		return;
+	}
+
+	stp->recent_used = true; //this is same wih upper one.
+
+	for (i = 0; i < PGSIZE/BLOCK_SECTOR_SIZE; i++){
+		block_read (swap_block, index*PGSIZE/BLOCK_SECTOR_SIZE + i, (uint8_t *) upage + i*BLOCK_SECTOR_SIZE);
+	}
+    lock_release(&swap_lock);
+}
+
+//swap out from memory
+size_t swap_out(void *upage)
+{
+
+}
+
+//find swap table pack with index. return NULL when can't find
+struct swap_table_pack* index_to_swap_table_pack(index)
+{
+    struct list_elem *e;
+    struct swap_table_pack *spt;
+    
+	if(list_empty(&swap_list)){
+        return NULL;
+    }
+	
+    for(e = list_begin(&swap_list); e != list_end(&swap_list); e = list_next(e)){
+		spt = list_entry(e, struct swap_table_pack, elem);
+		if(spt->index == index){
+            return spt;
+        }
+    }
+    return NULL;
 }
 
 //check swap table twice.
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -15,18 +15,22 @@ enum swap_type
 struct list swap_table;
 struct block *swap_block;
 struct lock swap_lock;
-struct list_elem *lru_pos; //pointer for saving where we find lru
+struct list_elem *lru_pos; //pointer for saving where we find lru. it looks unstable.
 
 struct swap_table_pack
 {
     struct sp_table_pack *sptp;
+    struct size_t index;
+    enum swap_type type;
     bool recent_used;
-    struct size_t idx;
-
+    
     struct list_elem elem;
 };
 
 void init_swap_table();
 struct swap_table_pack *find_lru_stp();
+struct swap_table_pack* index_to_swap_table_pack(size_t index);
+void swap_in(size_t index, void *upage);
+size_t swap_out(void *upage);
 
 #endif
