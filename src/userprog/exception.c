b42ed3f12fd461ef2e234e97ffa77a50525a58f8
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-11-24 19:42:15 +0900

    gwa

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -156,7 +156,7 @@ page_fault (struct intr_frame *f)
 
   if(!not_present || !check_addr_safe(fault_addr,1,NULL)) //not present check : pt-write-code test
   {
-    printf("fault_addr: %d = %x\n", fault_addr, f->eip);
+    printf("fault_addr: %d = %x\n", fault_addr, fault_addr);
     sys_exit(-99);
   }
 
@@ -172,7 +172,7 @@ page_fault (struct intr_frame *f)
   {
     if(!check_addr_safe(fault_addr,2,f->esp)) //stack verification
     {
-      printf("fault_addr: %d = %x\n", fault_addr, f->eip);
+      printf("fault_addr: %d = %x\n", fault_addr, fault_addr);
       sys_exit(-11);
     }
      
