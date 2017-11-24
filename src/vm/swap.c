24f93a3649aca2af605bd243f25417f79298a381
imnotkind <billy307@daum.net>
2017-11-24 13:09:49 +0900

    fas

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -72,12 +72,12 @@ int swap_out(void *upage)
 			break;
 		}
 	}
-/*
+
 	if(index == list_size(&swap_table)){
 		lock_release(&swap_lock);
 		return -1;
 	}
-*/
+
 	for (i = 0; i < SECTORS_PER_PAGE; i++) {
 		block_write (swap_block, index * SECTORS_PER_PAGE + i, (uint8_t *) upage + i*BLOCK_SECTOR_SIZE);
     }
