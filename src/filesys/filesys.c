749e5676a678db3a872a3641adfcaea0f21f983d
DESKTOP-LUE32S0\Changkeun Song <pckdrms@naver.com>
2017-12-14 22:36:07 +0900

    filesys

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -9,6 +9,8 @@
 #include "filesys/cache.h"
 #include "threads/thread.h"
 
+#define PATH_MAX 256
+#define DIR_ENTRIES 32
 /* Partition that contains the file system. */
 struct block *fs_device;
 
@@ -49,14 +51,25 @@ filesys_done (void)
    Fails if a file named NAME already exists,
    or if internal memory allocation fails. */
 bool
-filesys_create (const char *name, off_t initial_size) 
+filesys_create (const char *path, off_t initial_size, bool is_dir) 
 {
   block_sector_t inode_sector = 0;
-  struct dir *dir = dir_open_root ();
-  bool success = (dir != NULL
-                  && free_map_allocate (1, &inode_sector)
-                  && inode_create (inode_sector, initial_size, 0) //not dir
-                  && dir_add (dir, name, inode_sector));
+  char name[NAME_MAX + 1]; //to contain \0
+  struct dir *dir = parse_path(path, name);
+  bool success
+  if(is_dir){ // when it is dir
+    success = (dir != NULL
+              && free_map_allocate (1, &inode_sector)
+              && dir_create (inode_sector, DIR_ENTRIES) //not dir
+              && dir_add (dir, name, inode_sector));
+  }
+  else{ // when it is file
+    success = (dir != NULL
+              && free_map_allocate (1, &inode_sector)
+              && inode_create (inode_sector, initial_size, 0) //not dir
+              && dir_add (dir, name, inode_sector));
+  }
+
   if (!success && inode_sector != 0) 
     free_map_release (inode_sector, 1);
   dir_close (dir);
@@ -70,9 +83,10 @@ filesys_create (const char *name, off_t initial_size)
    Fails if no file named NAME exists,
    or if an internal memory allocation fails. */
 struct file *
-filesys_open (const char *name)
+filesys_open (const char *path)
 {
-  struct dir *dir = dir_open_root ();
+  char name[NAME_MAX + 1]; //to contain \0
+  struct dir *dir = parse_path (path, name);
   struct inode *inode = NULL;
 
   if (dir != NULL)
@@ -87,11 +101,26 @@ filesys_open (const char *name)
    Fails if no file named NAME exists,
    or if an internal memory allocation fails. */
 bool
-filesys_remove (const char *name) 
+filesys_remove (const char *path) 
 {
-  struct dir *dir = dir_open_root ();
-  bool success = dir != NULL && dir_remove (dir, name);
+  char name[NAME_MAX + 1], temp_dir[PATH_MAX+1]; //to contain \0
+  struct dir *dir, *cur_dir = NULL;
+  struct inode *inode;
+  bool success = false;
+
+  dir_lookup(dir, name, &inode);
+  dir = parse_path (path, name);
+
+  cur_dir = dir_open(inode);
+
+  if(!inode_is_dir(inode) || (cur && !dir_readdir(cur_dir, temp_dir)){
+    success = dir != NULL && dir_remove (dir, name);
+  }
   dir_close (dir); 
+  
+  if(cur_dir){
+    dir_close(cur_dir);
+  }
 
   return success;
 }
@@ -100,10 +129,17 @@ filesys_remove (const char *name)
 static void
 do_format (void)
 {
+  struct dir *root;
   printf ("Formatting file system...");
   free_map_create ();
   if (!dir_create (ROOT_DIR_SECTOR, 16))
     PANIC ("root directory creation failed");
+  
+  root = dir_open_root();
+  dir_add(root, ".", ROOT_DIR_SECTOR);
+  dir_add(root, "..", ROOT_DIR_SECTOR);
+  dir_close(root);
+
   free_map_close ();
   printf ("done.\n");
 }
